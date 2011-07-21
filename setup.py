d9543e507f10f05e0c818eb28003f2d715f6ddb7
Suchandra Thapa <sthapa@ci.uchicago.edu>
2011-07-21 22:08:44 +0000

    Initial setup.py file for distutils
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-configure/trunk@12344 4e558342-562e-0410-864c-e07659590f8c

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 from distutils.core import setup
+
+setup(name='osg-configure',
+      version='0.0.2',
+      description='Package for configure-osg and associated scripts',
+      author='Suchandra Thapa',
+      author_email='sthapa@ci.uchicago.edu',
+      url='http://www.opensciencegrid.org',
+      packages=['configure-osg'],
+      scripts=['scripts/configure-osg'],
+      data_files=['config', 'etc/osg']
+      )
