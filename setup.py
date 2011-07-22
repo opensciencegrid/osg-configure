5ad0b5ff3ecdf18b15613029aeefc27edda8c245
Suchandra Thapa <sthapa@ci.uchicago.edu>
2011-07-22 16:03:29 +0000

    Fixed up distutils setup for configure-osg
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-configure/trunk@12365 4e558342-562e-0410-864c-e07659590f8c

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
+import glob
+
+config_files = ['config/ce.ini',
+                'config/gums.ini',
+                'config/pigeon.ini',
+                'config/rsv.ini',
+                'config/storage.ini']
 
 setup(name='osg-configure',
       version='0.0.2',
@@ -6,7 +13,7 @@ setup(name='osg-configure',
       author='Suchandra Thapa',
       author_email='sthapa@ci.uchicago.edu',
       url='http://www.opensciencegrid.org',
-      packages=['configure-osg', 'configure-osg.modules', 'configure-osg.configure_modules'],      
+      packages=['configure_osg', 'configure_osg.modules', 'configure_osg.configure_modules'],      
       scripts=['scripts/configure-osg'],
-      #data_files=[('config', ['/etc/osg'])]
+      data_files=[('/etc/osg', config_files),]
       )
