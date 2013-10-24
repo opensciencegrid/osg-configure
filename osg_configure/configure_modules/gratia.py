786c82442c45f01b7482db5f297d5365157470e6
Suchandra Thapa <sthapa@ci.uchicago.edu>
2013-10-24 21:21:29 +0000

    Don't skip slurm config when slurm is enabled Improve messages when gratia jobmanager probe configuration is being skipped (i.e. indicate that gratia probe configuration is being skipped rather than just probe configuration)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-configure/trunk@18017 4e558342-562e-0410-864c-e07659590f8c

--- osg_configure/configure_modules/gratia.py
+++ osg_configure/configure_modules/gratia.py
@@ -142,18 +142,18 @@ in your config.ini file."""
             # if section is disabled then the following code won't work
             # since the parseConfiguration will shortcircuit, so 
             # give a warning and then move on
-            self.log("Skipping SGE probe configuration since SGE is disabled",
+            self.log("Skipping SGE gratia probe configuration since SGE is disabled",
                      level = logging.WARNING)
             continue
           sge_config = SGEConfiguration(logger = self.logger)
           sge_config.parseConfiguration(configuration)
           self.__probe_config['sge'] = {'sge_accounting_file' : sge_config.getAccountingFile()}
         elif probe == 'slurm':
-          if BaseConfiguration.sectionDisabled(configuration, 'Slurm'):
+          if BaseConfiguration.sectionDisabled(configuration, 'SLURM'):
             # if section is disabled then the following code won't work
             # since the parseConfiguration will shortcircuit, so 
             # give a warning and then move on
-            self.log("Skipping Slurm probe configuration since Slurm is disabled",
+            self.log("Skipping Slurm gratia probe configuration since Slurm is disabled",
                      level = logging.WARNING)
             continue
           slurm_config = SlurmConfiguration(logger = self.logger)
