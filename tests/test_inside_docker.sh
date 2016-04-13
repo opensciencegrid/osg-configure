fe7399ef5de4cafee5eee34e99bcf761f7c9baff
Derek Weitzel <djw8605@gmail.com>
2016-04-13 11:18:03 -0500

    Use Makefile rather than setup.py install

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -9,11 +9,12 @@ rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}
 yum -y install yum-plugin-priorities
 rpm -Uvh https://repo.grid.iu.edu/osg/3.3/osg-3.3-el${OS_VERSION}-release-latest.rpm
 
-yum -y install python condor-python
+yum -y install python condor-python make
 
 # First, install osg-configure
 cd /osg-configure
-python setup.py install
+make install
+#python setup.py install
 
 # Next, run the tests
 python tests/run-osg-configure-tests 
