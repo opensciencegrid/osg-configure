8b9a226117af98c633c372c35ae5fc8b7466c40e
Derek Weitzel <djw8605@gmail.com>
2016-04-13 11:13:57 -0500

    Adding OSG Repos

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+OS_VERSION=$1
+
+ls -l /home
+
+# First, install all the needed packages.
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm
+yum -y install yum-plugin-priorities
+rpm -Uvh https://repo.grid.iu.edu/osg/3.3/osg-3.3-el${OS_VERSION}-release-latest.rpm
+
+yum -y install python condor-python
+
 # First, install osg-configure
-yum -y install python
-
 cd /osg-configure
-
 python setup.py install
 
 # Next, run the tests
