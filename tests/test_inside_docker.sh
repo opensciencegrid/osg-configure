ebc4dcef6a7d676f9bbbe3a0f2ee10696a9f1121
Matyas Selmeci <matyas@cs.wisc.edu>
2020-09-03 16:21:59 -0500

    EL 8 travis tests (SOFTWARE-4191)

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -12,15 +12,21 @@ ls -l /home
 
 # First, install all the needed packages.
 rpm -U https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm
-yum -y install yum-plugin-priorities
+[[ $OS_VERSION -gt 7 ]] || yum -y install yum-plugin-priorities
 rpm -U https://repo.opensciencegrid.org/osg/${OSG_VERSION}/osg-${OSG_VERSION}-el${OS_VERSION}-release-latest.rpm
 
-yum -y install condor-python make
+if [[ $OS_VERSION -gt 7 ]]; then
+    yum -y install --enablerepo=osg-testing --enablerepo=PowerTools python3-condor python3 make
+    PYTHON=python3
+else
+    yum -y install python2-condor make
+    PYTHON=python2
+fi
 
 # First, install osg-configure
 cd /osg-configure
-make install
+make install PYTHON=$PYTHON
 
 # Next, run the tests
-python tests/run-osg-configure-tests
+$PYTHON tests/run-osg-configure-tests
 
