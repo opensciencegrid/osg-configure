dc5e894671990561dce9b7637015c96f7d2c266e
Matyas Selmeci <matyas@cs.wisc.edu>
2020-09-04 21:06:11 -0500

    Drop test_ini_spaces.  The format's changed and the test's not _that_ useful  (SOFTWARE-4191)

--- tests/test_configfile.py
+++ tests/test_configfile.py
@@ -194,20 +194,6 @@ class TestConfigFile(unittest.TestCase):
         self.assertFalse(configfile.jobmanager_enabled(config),
                          "jobmanager_enabled returned true on a config without an enabled jobmanager")
 
-    def test_ini_spaces(self):
-        """
-        Test to make sure ini files with spaces work correctly
-        """
-
-        config_dirs = [get_test_config('config-space1.d'),
-                       get_test_config('config-space2.d'),
-                       get_test_config('config-space3.d')]
-        temp = sys.stderr
-        sys.stderr = open(os.devnull, 'w')
-        for directory in config_dirs:
-            self.assertRaises(SystemExit, configfile.read_config_files, config_directory=directory)
-        sys.stderr = temp
-
 
 if __name__ == '__main__':
     unittest.main()
