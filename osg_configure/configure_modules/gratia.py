76667543a3370ff884d9eb1ef39bf09f13fa2681
Matyas Selmeci <matyas@cs.wisc.edu>
2021-02-19 12:18:45 -0600

    buncha renames so I can understand what's actually going on

--- osg_configure/configure_modules/gratia.py
+++ osg_configure/configure_modules/gratia.py
@@ -62,9 +62,8 @@ in your config.ini file."""
                                               default_value='',
                                               required=configfile.Option.OPTIONAL)}
 
-        # Dictionary holding probe settings, the probe's name is used as the key and the
-        # server the probe should report to is the value.
-        self.enabled_probe_settings = {}
+        # Dictionary of which host[:port] to send probe data to, keyed by probe.
+        self.enabled_probe_hosts = {}
 
         # defaults for itb and production use
         self._itb_defaults = {'probes':
@@ -117,8 +116,8 @@ in your config.ini file."""
                     self._itb_defaults['probes']
 
             # grab configuration information for various jobmanagers
-            probes = self.get_installed_probes()
-            for probe in probes:
+            probes_iter = self.get_installed_probe_config_files().keys()
+            for probe in probes_iter:
                 if probe == 'condor':
                     self._probe_config['condor'] = {'condor_location':
                                                          CondorConfiguration.get_condor_location(configuration),
@@ -197,7 +196,7 @@ in your config.ini file."""
             self.log('GratiaConfiguration.parse_configuration completed')
             return
 
-        self._parse_probes(self.options['probes'].value)
+        self._set_enabled_probe_host(self.options['probes'].value)
         self.log('GratiaConfiguration.parse_configuration completed')
 
     def configure(self, attributes):
@@ -236,8 +235,9 @@ in your config.ini file."""
             return False
 
         hostname = attributes['OSG_HOSTNAME']
-        probe_list = self.get_installed_probes()
-        for probe in probe_list:
+        probe_config_files = self.get_installed_probe_config_files()
+        probes_iter = probe_config_files.keys()
+        for probe in probes_iter:
             if probe in self._job_managers:
                 if probe not in self._probe_config:
                     # Probe is installed but we don't have configuration for it
@@ -245,18 +245,18 @@ in your config.ini file."""
                     # manager is not shared
                     continue
 
-                if 'jobmanager' in self.enabled_probe_settings:
-                    probe_host = self.enabled_probe_settings['jobmanager']
+                if 'jobmanager' in self.enabled_probe_hosts:
+                    probe_host = self.enabled_probe_hosts['jobmanager']
                 else:
                     continue
             else:
-                if probe in self.enabled_probe_settings:
-                    probe_host = self.enabled_probe_settings[probe]
+                if probe in self.enabled_probe_hosts:
+                    probe_host = self.enabled_probe_hosts[probe]
                 else:
                     continue
 
             self._make_subscription(probe,
-                                    probe_list[probe],
+                                    probe_config_files[probe],
                                     probe_host,
                                     self.options['resource'].value,
                                     hostname)
@@ -278,9 +278,9 @@ in your config.ini file."""
 
     # pylint: disable-msg=R0201
     @staticmethod
-    def get_installed_probes():
-        """
-        Check for probes that have been installed and return a list of these probes installed
+    def get_installed_probe_config_files():
+        """Return a mapping of probe name -> ProbeConfig file.
+        Note that "pbs" and "lsf" have the same probe.
         """
 
         probes = {}
@@ -316,7 +316,7 @@ in your config.ini file."""
             self.log("GratiaConfiguration.check_attributes completed")
             return True
         status = self._check_servers()
-        status &= self._verify_gratia_dirs()
+        status &= self._verify_gratia_dirs_for_condor_probe()
         self.log("GratiaConfiguration.check_attributes completed")
         return status
 
@@ -395,11 +395,11 @@ in your config.ini file."""
         e.g. server1.example.com,server2.example.com:2188
         """
         valid = True
-        for probe in self.enabled_probe_settings:
+        for probe in self.enabled_probe_hosts:
             if probe == 'metric':
                 sys.stdout.write(self.metric_probe_deprecation + "\n")
                 self.log(self.metric_probe_deprecation, level=logging.WARNING)
-            server = self.enabled_probe_settings[probe].split(':')[0]
+            server = self.enabled_probe_hosts[probe].split(':')[0]
             if not validation.valid_domain(server, False):
                 err_mesg = "The server specified for probe %s is not " % probe
                 err_mesg += "a valid domain: %s" % server
@@ -409,8 +409,8 @@ in your config.ini file."""
                 err_mesg = "The server specified for probe %s does not " % probe
                 err_mesg += "resolve: %s" % server
                 self.log(err_mesg, level=logging.WARNING)
-            if server != self.enabled_probe_settings[probe]:
-                port = self.enabled_probe_settings[probe].split(':')[1]
+            if server != self.enabled_probe_hosts[probe]:
+                port = self.enabled_probe_hosts[probe].split(':')[1]
                 try:
                     temp = int(port)
                     if temp < 0:
@@ -422,10 +422,11 @@ in your config.ini file."""
                              level=logging.ERROR)
         return valid
 
-    def _parse_probes(self, probes):
-        """
-        Parse a list of probes and set the list of enabled probes for this
-        configuration
+    def _set_enabled_probe_host(self, probes):
+        """Parse a list of probes (taken from the Gratia.probes option)
+        and set the `enabled_probe_hosts`, which is a probe name -> upload host
+        mapping (with an optional port).
+        Treat "gridftp" as an alias for "gridftp-transfer".
         """
 
         for probe_entry in probes.split(','):
@@ -434,9 +435,9 @@ in your config.ini file."""
             if probe_name == 'gridftp':
                 probe_name = 'gridftp-transfer'
             if len(tmp[1:]) == 1:
-                self.enabled_probe_settings[probe_name] = tmp[1]
+                self.enabled_probe_hosts[probe_name] = tmp[1]
             else:
-                self.enabled_probe_settings[probe_name] = ':'.join(tmp[1:])
+                self.enabled_probe_hosts[probe_name] = ':'.join(tmp[1:])
 
     def _auto_configure(self, configuration):
         """
@@ -471,7 +472,7 @@ in your config.ini file."""
                                           'OSG or OSG-ITB')
 
         self.options['probes'].value = probes
-        self._parse_probes(probes)
+        self._set_enabled_probe_host(probes)
 
         return True
 
@@ -602,7 +603,7 @@ in your config.ini file."""
         return True
         
 
-    def _verify_gratia_dirs(self):
+    def _verify_gratia_dirs_for_condor_probe(self):
         """
         Verify that the condor per_job_history directory and the DataFolder
         directory are the same and warn if admin if the two don't match
@@ -632,7 +633,7 @@ in your config.ini file."""
             data_folder = data_folder.strip('" \t')
             # PER_JOB_HISTORY_DIR comes from the schedd, so if condor's not
             # running, we can't get a value (SOFTWARE-1564)
-            history_dir = self._get_history_dir(condor_config_val_bin)
+            history_dir = self._get_condor_history_dir(condor_config_val_bin)
             if not history_dir:
                 self.log("Could not verify DataFolder correctness: unable to get PER_JOB_HISTORY_DIR. "
                          "This may be caused by the condor schedd not running, or by PER_JOB_HISTORY_DIR "
@@ -671,7 +672,7 @@ in your config.ini file."""
 
         return valid
 
-    def _get_history_dir(self, condor_config_val_bin):
+    def _get_condor_history_dir(self, condor_config_val_bin):
         cmd = [condor_config_val_bin, '-schedd', 'PER_JOB_HISTORY_DIR']
         try:
             process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="latin-1")
